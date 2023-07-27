4a987676e87cea3b562384ea141cd02b25e142d3
liubo <1095796478@qq.com>
2023-07-27 15:48:46 +0800

    修复inpens hover 报错

--- packages/core/src/canvas/canvas.ts
+++ packages/core/src/canvas/canvas.ts
@@ -2663,7 +2663,7 @@ export class Canvas {
         if (pen.children) {
           const pens = []; // TODO: 只考虑了一级子
           pen.children.forEach((id) => {
-            pens.push(this.store.pens[id]);
+            this.store.pens[id] && pens.push(this.store.pens[id]);
           });
           hoverType = this.inPens(pt, pens);
           if (hoverType) {
